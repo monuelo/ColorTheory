c9b0ace5bee2ce1e977caea10c35dfdf02660c87
Héricles Emanuel <hericles.me@gmail.com>
2019-03-16 02:19:51 -0300

    Colors component

---
+++ src/components/Colors/Colors.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Color from '../Color/Color';
+import ColorsData from '../../data/colors';
+import styles from './Colors.module.css';
+
+class Colors extends Component {
+
+    render() {
+        return (
+            <div className={styles.colors}>
+                {Object.values(ColorsData)
+                    .map((color, index) => <Color key={index} color={color} />)}
+            </div>
+        );
+    }
+}
+
+export default Colors;  
