e52676f931e7ddf157f5eabbe628290c11432d0d
Héricles Emanuel <hericles.me@gmail.com>
2019-03-16 02:21:06 -0300

    render colors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Colors from './components/Colors/Colors';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  render = () => <Colors/>
 }
 
 export default App;
