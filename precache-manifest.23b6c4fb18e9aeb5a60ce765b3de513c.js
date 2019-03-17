88765d46715c76df3fedbd4aafd3eada639ff711
Héricles Emanuel <hericles.me@gmail.com>
2019-03-17 14:20:31 -0300

    Updates

---
+++ precache-manifest.23b6c4fb18e9aeb5a60ce765b3de513c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8416535750e8398ab33",
+    "url": "/static/js/main.67c7c5a6.chunk.js"
+  },
+  {
+    "revision": "2c24b51e99eeb78723e8",
+    "url": "/static/js/2.e49df853.chunk.js"
+  },
+  {
+    "revision": "f8416535750e8398ab33",
+    "url": "/static/css/main.7897975d.chunk.css"
+  },
+  {
+    "revision": "3849cca7263fbcd8fb3fee05ee5532c5",
+    "url": "/index.html"
+  }
+];
